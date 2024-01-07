10a8e185e89b515ca91f8a91bf3a6c33f0650d88
Mykola <mykola.chmut@codeminers.com.ua>
2024-01-07 17:53:17 +0200

    Wipdawsd

--- prisma/migrations/20240107152409_init/migration.sql
+++ prisma/migrations/20240107152409_init/migration.sql
@@ -1,7 +1,7 @@
 /*
   Warnings:
 
-  - You are about to drop the column `details` on the `user` table. All the data in the column will be lost.
+  - Added the required column `name` to the `user` table without a default value. This is not possible if the table is not empty.
 
 */
 -- RedefineTables
@@ -10,6 +10,7 @@ CREATE TABLE "new_user" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "login" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "creationDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 INSERT INTO "new_user" ("creationDate", "id", "login", "password") SELECT "creationDate", "id", "login", "password" FROM "user";
