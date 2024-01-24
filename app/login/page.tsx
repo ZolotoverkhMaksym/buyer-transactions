9977e044bc537c86a7b11c15e5f78edb68d24efd
Maksym Zolotoverkh <zolotoverhmaksim@gmail.com>
2024-01-24 13:38:53 +0200

    +

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import styles from './page.module.scss'
 
 interface LoginForm {
-  login: string; 
+  login: string;
   password: string;
 }
 
 export default function Home() {
-  const [form, setForm] = useState<LoginForm>({login: '', password: ''}); 
+  const [form, setForm] = useState<LoginForm>({ login: '', password: '' });
 
 
   const handleLoginChange = (login: string) => {
@@ -30,24 +30,24 @@ export default function Home() {
   }
 
   const submit = () => {
-console.log(form)
+    console.log(form)
   }
-  
+
   return (
     <div className={styles.logins}>
       <div className={styles.title}>Sign in to my Profile</div>
       <div className={styles.box}>
-      <button className={styles.google} onClick={submit}><div className={styles.icon}></div>Continue with Google</button>
-      <span className={styles.span}> or </span>
-      <div className={styles.span}> or </div>
-     <input className={styles.input} value={form.login} onChange={event => handleLoginChange(event.target.value)} placeholder="Login" type="login"/>
-     <input className={styles.input} value={form.password} onChange={event => handlePasswordChange(event.target.value)} placeholder="Password" type="password"/>
-     <button className={styles.button} onClick={submit}>Log in</button>
-     <div className={styles.title}></div>
-     <div><a className={styles.blue_link} href="#">Use single sing-on</a></div>
-     <div><a className={styles.blue_link} href="#">Reset password</a></div>
-     <div className={styles.silver}>No account?&nbsp;<a className={styles.blue_link} href="#">Create one</a></div>
-     </div>
+        <button className={styles.google} onClick={submit}><div className={styles.icon}></div>Continue with Google</button>
+        <span className={styles.span}> or </span>
+        <div className={styles.span}> or </div>
+        <input className={styles.input} value={form.login} onChange={event => handleLoginChange(event.target.value)} placeholder="Login" type="login" />
+        <input className={styles.input} value={form.password} onChange={event => handlePasswordChange(event.target.value)} placeholder="Password" type="password" />
+        <button className={styles.button} onClick={submit}>Log in</button>
+        <div className={styles.title}></div>
+        <div><a className={styles.blue_link} href="#">Use single sing-on</a></div>
+        <div><a className={styles.blue_link} href="#">Reset password</a></div>
+        <div className={styles.silver}>No account?&nbsp;<a  className={styles.blue_link} href="/Create one">Create one</a></div>
+      </div>
     </div>
   )
 }
